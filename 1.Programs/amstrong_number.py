745517cae9a6378c7c2f76b72a3a001eba51cd46
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2020-08-26 13:00:41 +0530

    basic programs
    Signed-off-by: Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>

--- 1.Programs/amstrong_number.py
+++ 1.Programs/amstrong_number.py
@@ -1,6 +1,12 @@
-def isAmstrongNumber(num):
+def checkIfNumberIs(type,num):
+    multiplynum = 3
     total = 0
     divnum = num
+    if type == 'armstrong':
+        multiplynum = 3
+    elif type == 'narcistic':
+        multiplynum = 4
+
     while divnum > 0:
         #print(divnum)
         modnum = divnum % 10
@@ -14,4 +20,9 @@ def isAmstrongNumber(num):
     else:
         return False
 
-print(isAmstrongNumber(152))
+print(checkIfNumberIs('armstrong',152))
+print(checkIfNumberIs('armstrong',153))
+
+
+print(checkIfNumberIs('narcistic',152))
+print(checkIfNumberIs('narcistic',153))
